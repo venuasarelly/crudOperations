3e8bcc89325f6d74e054aa9b1859a5f1455e20d3
venuasarelly <asarellyvenusiri1@gmail.com>
2023-02-28 16:26:26 +0530

    first commit

---
+++ server/model/model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact : {
+    type : String,
+    required: true,
+    },
+    gender : String,
+    status : String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
